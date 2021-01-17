c36c585073a21c43ce24b0997df8f86f5442d256
Claudio Tome <cptome@gmail.com>
2021-01-16 21:23:04 -0300

    funcionando

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+  
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `cocktail_Id` to associate dose with corresponding cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+     redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Whisky")
+Ingredient.create(name: "Coca")
+Ingredient.create(name: "Fernet")
+Ingredient.create(name: "Sprite")
