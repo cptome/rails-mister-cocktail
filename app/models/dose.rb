7b6689647bdbc2db120f0be6fa01b0d313d2f5eb
Claudio Tome <cptome@gmail.com>
2021-01-21 20:29:55 -0300

    varias fotos en cada trago agregadas

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail
-  belongs_to :ingredient
+  belongs_to :cocktail, foreign_key: "cocktail_id"
+  belongs_to :ingredient, foreign_key: "ingredient_id"
 
   validates :description, presence: true
 
